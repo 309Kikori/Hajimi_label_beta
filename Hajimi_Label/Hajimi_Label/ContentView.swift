5953a985d1b5f12ae354c2aeae0d7bba6806be8d
Quantize <161130485+309Kikori@users.noreply.github.com>
2025-12-17 22:05:52 +0800

    update #6 macOS

---
+++ Hajimi_Label/Hajimi_Label/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hajimi_Label
+//
+//  Created by shinonome on 17/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hajimi_Label/Hajimi_Label/Hajimi_LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hajimi_LabelApp.swift
+//  Hajimi_Label
+//
+//  Created by shinonome on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hajimi_LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
